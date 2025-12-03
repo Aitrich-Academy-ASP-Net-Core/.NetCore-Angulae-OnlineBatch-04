cd43f669854de6b1e9b654578acc6ed55f672523
apsara123127 <apsaraaitrich@gmail.com>
2025-12-03 15:58:50 +0530

    added

--- HTTP Crud/httpSample/src/app/app.component.ts
+++ HTTP Crud/httpSample/src/app/app.component.ts
@@ -12,6 +12,7 @@ export class AppComponent implements OnInit {
 
   name:string = '';
   age:string = '';
+  editingId: number | null = null;
 
   // students must be an array for *ngFor
   students:any[] = [];
@@ -29,17 +30,49 @@ export class AppComponent implements OnInit {
   }
 
   addStudent() {
-    this.http.post<any>('http://localhost:3000/students',
-      { name: this.name, age: this.age })
-      .subscribe(stu => {
+    if (this.editingId == null) {
+      // CREATE (POST)
+      this.http.post<any>('http://localhost:3000/students',
+        { name: this.name, age: this.age })
+        .subscribe(stu => {
+          this.loadStudents();     // refresh list
+          this.clearForm();        // clear inputs
+        });
+    } else {
+      // UPDATE (PUT)
+      this.updateStudent(this.editingId, { name: this.name, age: this.age });
+    }
+  }
 
-        // Add new student to list
-        this.loadStudents();
-
-        // Clear form
-        this.name = '';
-        this.age = '';
+  // PUT - update a student by id
+  updateStudent(id: number, body: { name: string, age: string }) {
+    this.http.put<any>(`http://localhost:3000/students/${id}`, body)
+      .subscribe(res => {
+        this.loadStudents();     // refresh list after update
+        this.clearForm();        // clear and leave edit mode
       });
+  }
+
+  // DELETE - remove student by id
+  deleteStudent(id: number) {
+    this.http.delete<any>(`http://localhost:3000/students/${id}`)
+      .subscribe(res => {
+        this.loadStudents();     // refresh list after delete
+      });
+  }
+
+  // small helper: start editing a student -> load into form
+  startEdit(student: any) {
+    this.editingId = student.id;  // remember which id we are editing
+    this.name = student.name;
+    this.age = student.age;
+  }
+
+  // clear form and cancel editing
+  clearForm() {
+    this.name = '';
+    this.age = '';
+    this.editingId = null;
   }
 
 }
